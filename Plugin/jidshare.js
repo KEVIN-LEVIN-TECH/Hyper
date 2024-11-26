9ffd76c85aa6764996ad792c4753dfc8d065328c
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-26 14:34:14 +0530

    Update jidshare.js

--- Plugin/jidshare.js
+++ Plugin/jidshare.js
@@ -33,7 +33,7 @@ async (conn, mek, m, { from, q, reply }) => {
         }
 
         const movie = movieDetails.result;
-        let movieMessage = `*${movie.title}*\n\n`;
+        let movieMessage = `${movie.title}\n\n`;
         movieMessage += `📅 𝖱𝖾𝗅𝖾𝖺𝗌𝖾 𝖣𝖺𝗍𝖾: ${movie.release_date}\n`;
         movieMessage += `🗺 𝖢𝗈𝗎𝗇𝗍𝗋𝗒: ${movie.country}\n`;
         movieMessage += `⏰ 𝖣𝗎𝗋𝖺𝗍𝗂𝗈𝗇: ${movie.duration}\n`;
@@ -45,11 +45,11 @@ async (conn, mek, m, { from, q, reply }) => {
         movieMessage += `⭐ 𝖨𝗆𝖽𝖻 𝖱𝖺𝗍𝗂𝗇𝗀: ${movie.IMDb_Rating}\n`;
         movieMessage += `🎬 𝖣𝗂𝗋𝖾𝖼𝗍𝗈𝗋: ${movie.director.name}\n\n`;
          movieMessage += `🔗 𝖣𝗈𝗐𝗇𝗅𝗈𝖺𝖽 𝖫𝗂𝗇𝗄: ${link}`;
-        movieMessage += `\n\n*乂 REPLY BELOW NUMBER*\n\n`;
-        movieMessage += `*1 | 𝖲𝖣 - 480𝗉*\n`;
-        movieMessage += `*2 | 𝖧𝖣 - 720p*\n`;
-        movieMessage += `*3 | 𝖥𝖧𝖣 - 1080p*\n\n`;
-        movieMessage += `> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜ*`;
+        movieMessage += `\n\n乂 REPLY BELOW NUMBER\n\n`;
+        movieMessage += `1 | 𝖲𝖣 - 480𝗉\n`;
+        movieMessage += `2 | 𝖧𝖣 - 720p\n`;
+        movieMessage += `3 | 𝖥𝖧𝖣 - 1080p\n\n`;
+        movieMessage += `> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ `;
 
         const imageUrl = movie.images && movie.images.length > 0 ? movie.images[0] : null;
 
@@ -90,8 +90,8 @@ async (conn, mek, m, { from, q, reply }) => {
                             url: directLink
                         },
                         mimetype: 'video/mp4',
-                        fileName: `🎬ꜱᴀʜᴀꜱ-ᴍᴅ ᴍᴏᴠɪᴇꜱ🎬(${movie.title}).mp4`,
-                        caption: `${movie.title} - ${quality}\n\n> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜ*`
+                        fileName: `🎬ʜʏᴘᴇʀ-ᴍᴅ ᴍᴏᴠɪᴇꜱ🎬(${movie.title}).mp4`,
+                        caption: `${movie.title} - ${quality}\n\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ `
                     });
                 }
             }
