902c3b9948513dcf5c76a12cdb14f7d2f315607f
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-26 15:08:37 +0530

    Update scloud.js

--- Plugin/scloud.js
+++ Plugin/scloud.js
@@ -8,18 +8,18 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("_*please give me a valid url ❗*_")
-if (!q.includes('soundcloud.com')) return reply("_*That's is not soundcloud url ❕*_")
+if(!q) return reply("_please give me a valid url ❗_")
+if (!q.includes('soundcloud.com')) return reply("_That's is not soundcloud url ❕_")
 const response = await fetch(`https://prabath-md-api.up.railway.app/api/sclouddl?url=${q}`)
 const data = await response.json()
 const fbvid = data.data.sd
-reply("_*Downloading your *SOUND CLOUD AUDIO*＿＿📥*_")
-reply("_*Uploading your *SOUND CLOUD AUDIO* ＿＿📤*_")
-await conn.sendMessage(from,{video : {url : sclouddl },caption : `┌────────────\n* SOUND CLOUD ᴅᴏᴡɴʟᴏᴀᴅᴇʀ..✅*\n> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜ*\n└─────────────`, mimetype:"audio/mpeg"},{quoted:mek})
+reply("_Downloading your SOUND CLOUD AUDIO＿＿📥_")
+reply("_Uploading your SOUND CLOUD AUDIO ＿＿📤_")
+await conn.sendMessage(from,{video : {url : sclouddl },caption : `┌────────────\nSOUND CLOUD ᴅᴏᴡɴʟᴏᴀᴅᴇʀ..✅\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ \n└─────────────`, mimetype:"audio/mpeg"},{quoted:mek})
 
 }catch(e){
     console.log(e)
-    reply("_*SCLOUD downloader sever are busy now 🛜*_\n_*please wait few minutes and try again 🔄*_")
+    reply("_*SCLOUD downloader sever are busy now 🛜_\n_please wait few minutes and try again 🔄_")
 }
 }
 )
